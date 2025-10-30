bd959253a8da0390c0a320c6664837440dc54173
David Cohen <davidfcohen@protonmail.com>
2025-10-30 10:24:40 -0400

    finish api implementation

--- src/api.rs
+++ src/api.rs
@@ -5,13 +5,26 @@ use std::{
     sync::Arc,
 };
 
+use async_trait::async_trait;
 use tokio::signal;
-use tonic::transport::{Error as TransportError, Server};
+use tonic::{
+    Request, Response, Result, Status,
+    transport::{Error as TransportError, Server as InnerServer},
+};
 use tonic_reflection::server::{Builder as ReflectionBuilder, Error as ReflectionError};
 use tracing::info;
 
-use crate::wallet;
-use proto::FILE_DESCRIPTOR_SET;
+use crate::wallet::{self, WalletError, WalletErrorKind};
+use proto::{
+    BalanceRequest, BalanceResponse, FILE_DESCRIPTOR_SET, ListResponse, TrackRequest,
+    UntrackRequest, Wallet,
+    wallet_service_server::{WalletService, WalletServiceServer},
+};
+
+mod proto {
+    pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("descriptor");
+    tonic::include_proto!("wallet.v1");
+}
 
 #[derive(Debug)]
 pub struct ApiError(Box<dyn error::Error + Send + Sync + 'static>);
@@ -61,13 +74,13 @@ impl fmt::Debug for Controller {
 }
 
 #[derive(Debug, Clone)]
-pub struct ApiServer {
+pub struct Server {
     controller: Controller,
     addr: Option<IpAddr>,
     port: Option<u16>,
 }
 
-impl ApiServer {
+impl Server {
     pub fn new(controller: Controller) -> Self {
         Self {
             controller,
@@ -97,15 +110,20 @@ impl ApiServer {
             50051
         });
 
-        let reflection = ReflectionBuilder::configure()
+        let server_reflection = ReflectionBuilder::configure()
             .register_encoded_file_descriptor_set(FILE_DESCRIPTOR_SET)
             .build_v1()?;
+
+        let server_wallet = WalletServiceServer::new(WalletServer {
+            controller: self.controller,
+        });
 
         let socket = SocketAddr::new(addr, port);
         info!("started grpc server on {socket}");
 
-        Server::builder()
-            .add_service(reflection)
+        InnerServer::builder()
+            .add_service(server_reflection)
+            .add_service(server_wallet)
             .serve_with_shutdown(socket, capture_shutdown_signal())
             .await?;
 
@@ -140,7 +158,113 @@ async fn capture_shutdown_signal() {
     info!("received shutdown signal")
 }
 
-mod proto {
-    pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("descriptor");
-    tonic::include_proto!("wallet.v1");
+#[derive(Debug, Clone)]
+pub struct WalletServer {
+    pub controller: Controller,
+}
+
+#[async_trait]
+impl WalletService for WalletServer {
+    async fn balance(&self, request: Request<BalanceRequest>) -> Result<Response<BalanceResponse>> {
+        let name = request
+            .into_inner()
+            .name
+            .ok_or(Status::invalid_argument("missing required name"))?;
+
+        let balance = self
+            .controller
+            .wallet_balance
+            .execute(&name)
+            .await
+            .map_err(|e| error_to_status(&e))?;
+
+        Ok(Response::new(BalanceResponse {
+            balance: Some(balance),
+        }))
+    }
+
+    async fn list(&self, _request: Request<()>) -> Result<Response<ListResponse>> {
+        let wallets = self
+            .controller
+            .wallet_list
+            .execute()
+            .await
+            .map_err(|e| error_to_status(&e))?;
+
+        let wallets = wallets
+            .into_iter()
+            .map(|w| Wallet {
+                name: Some(w.name),
+                address: Some(w.address),
+            })
+            .collect();
+
+        Ok(Response::new(ListResponse { wallet: wallets }))
+    }
+
+    async fn track(&self, request: Request<TrackRequest>) -> Result<Response<()>> {
+        let name = request
+            .into_inner()
+            .name
+            .ok_or(Status::invalid_argument("missing required name"))?;
+
+        let address = request
+            .into_inner()
+            .address
+            .ok_or(Status::invalid_argument("missing required address"))?;
+
+        self.controller
+            .wallet_track
+            .execute(&name, &address)
+            .await
+            .map_err(|e| error_to_status(&e))?;
+
+        Ok(Response::new(()))
+    }
+
+    async fn untrack(&self, request: Request<UntrackRequest>) -> Result<Response<()>> {
+        let name = request
+            .into_inner()
+            .name
+            .ok_or(Status::invalid_argument("missing required name"))?;
+
+        self.controller
+            .wallet_untrack
+            .execute(&name)
+            .await
+            .map_err(|e| error_to_status(&e))?;
+
+        Ok(Response::new(()))
+    }
+}
+
+fn error_to_status(error: &WalletError) -> Status {
+    let message = compose_error(error);
+
+    match error.kind() {
+        WalletErrorKind::NotFound => Status::not_found(message),
+        WalletErrorKind::NameConflict => Status::already_exists(message),
+        WalletErrorKind::NameEmpty => Status::invalid_argument(message),
+        WalletErrorKind::NameTooLong => Status::invalid_argument(message),
+        WalletErrorKind::WalletStore => Status::internal(message),
+        WalletErrorKind::WalletChain => Status::internal(message),
+        WalletErrorKind::WalletAddrParse => Status::invalid_argument(message),
+    }
+}
+
+fn compose_error(error: &dyn std::error::Error) -> String {
+    let mut composed = error.to_string();
+
+    let mut next: &dyn std::error::Error = &error;
+    while let Some(source) = next.source() {
+        composed.push_str(": ");
+        composed.push_str(&source.to_string());
+        next = source;
+    }
+
+    composed
+}
+
+fn missing_required(arg: &str) -> Status {
+    Status::invalid_argument(format!("missing required {arg}"))
 }
